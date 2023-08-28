34aa784b49cfdef7bd3fa8b6e0cd6c54be8f59cf
Moein <tje3d@yahoo.com>
2023-08-29 02:34:42 +0330

    :sparkles: feat: Add error and better style for inputText

--- src/components/form/input-text.svelte
+++ src/components/form/input-text.svelte
@@ -12,18 +12,24 @@
   export let onlyNumber: boolean = false
   export let maxLength: number = 110
   export let password: boolean = false
+  export let error: string | undefined = undefined
 
-  let input: HTMLInputElement
+  let input: HTMLInputElement | HTMLTextAreaElement
+  let isEmpty = true
+
+  $: isEmpty = (value + '').length === 0
 
   function onFocus(this: HTMLInputElement | HTMLTextAreaElement) {
     this.select()
   }
 
   onMount(() => {
-    if (password) {
-      input.type = 'password'
-    } else {
-      input.type = 'text'
+    if (input instanceof HTMLInputElement) {
+      if (password) {
+        input.type = 'password'
+      } else {
+        input.type = 'text'
+      }
     }
   })
 </script>
@@ -32,7 +38,8 @@
   {#if !textArea}
     <input
       bind:this={input}
-      class="peer h-full w-full rounded-[7px] border border-blue-gray-200 hide-border-t-notempty bg-transparent px-3 py-2.5 text-sm font-normal text-blue-gray-700 dark:text-blue-gray-200 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 dark:placeholder-shown:border-gray-600 dark:placeholder-shown:border-t-gray-600 focus:border-2 focus:border-blue-500 dark:focus:border-blue-500 focus:border-t-transparent dark:focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 dark:disabled:bg-white/5"
+      class="peer h-full w-full rounded-[7px] border border-gray-200 hide-border-t-notempty bg-transparent px-3 py-2.5 text-sm font-normal text-gray-700 dark:text-gray-200 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-gray-200 placeholder-shown:border-t-gray-200 dark:placeholder-shown:border-gray-600 dark:placeholder-shown:border-t-gray-600 focus:border-2 focus:border-blue-500 dark:focus:border-blue-500 focus:border-t-transparent dark:focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-gray-50 dark:disabled:bg-white/5"
+      class:border-gray-400={!isEmpty}
       placeholder=" "
       type="text"
       maxlength={maxLength}
@@ -44,14 +51,22 @@
     />
   {:else}
     <textarea
-      class="peer h-full w-full rounded-[7px] border border-blue-gray-200 hide-border-t-notempty bg-transparent px-3 py-2.5 text-sm font-normal text-blue-gray-700 dark:text-blue-gray-200 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 dark:placeholder-shown:border-gray-600 dark:placeholder-shown:border-t-gray-600 focus:border-2 focus:border-blue-500 dark:focus:border-blue-500 focus:border-t-transparent dark:focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 dark:disabled:bg-white/5 pt-4"
+      bind:this={input}
+      class="peer h-full w-full rounded-[7px] border border-gray-200 hide-border-t-notempty bg-transparent px-3 py-2.5 text-sm font-normal text-gray-700 dark:text-gray-200 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-gray-200 placeholder-shown:border-t-gray-200 dark:placeholder-shown:border-gray-600 dark:placeholder-shown:border-t-gray-600 focus:border-2 focus:border-blue-500 dark:focus:border-blue-500 focus:border-t-transparent dark:focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-gray-50 dark:disabled:bg-white/5 pt-4"
+      class:border-gray-400={!isEmpty}
       placeholder=" "
       on:focus={onFocus}
     />
   {/if}
   <label
-    class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:ml-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tr-md before:border-t before:border-r before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:mr-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tl-md after:border-t after:border-l after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.75] peer-placeholder-shown:text-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-r-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-l-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-gray-500"
+    class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:ml-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tr-md before:border-t before:border-r before:border-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:mr-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tl-md after:border-t after:border-l after:border-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.75] peer-placeholder-shown:text-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-500 peer-focus:before:border-t-2 peer-focus:before:border-r-2 peer-focus:before:border-blue-500 peer-focus:after:border-t-2 peer-focus:after:border-l-2 peer-focus:after:border-blue-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-gray-500"
+    class:before:border-gray-400={!isEmpty}
+    class:after:border-gray-400={!isEmpty}
   >
     {label}
   </label>
 </div>
+
+{#if error}
+  <div class="font-bold text-xs text-red-500 mt-2">{error}</div>
+{/if}
