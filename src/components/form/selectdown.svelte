b5588d4a570b0ba08fb7cdba3cdc0fd5d093c521
Moein <tje3d@yahoo.com>
2023-09-01 16:11:45 +0330

    fix: Textarea styles

--- src/components/form/selectdown.svelte
+++ src/components/form/selectdown.svelte
@@ -122,6 +122,7 @@
           ? 'border border-blue-gray-200 border-t-blue-gray-200 dark:border-gray-600 dark:border-t-gray-600'
           : 'border-t-transparent focus:border-t-transparent dark:focus:border-t-transparent'
       } ${disabled ? 'border-0 bg-blue-gray-50' : ''}`}
+      class:border-gray-400={!$isEmpty}
       use:Ripple
       tabindex="0"
       on:focus={onFocus}
@@ -148,7 +149,7 @@
       </div>
     </div>
     <label
-      class={`before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:ml-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tr-md before:border-t before:border-r before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:mr-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tl-md after:border-t after:border-l after:border-blue-gray-200 after:transition-all ${
+      class={`before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal text-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:ml-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tr-md before:border-t before:border-r before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:mr-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tl-md after:border-t after:border-l after:border-blue-gray-200 after:transition-all ${
         $isEmpty ? 'text-sm text-gray-500 before:border-transparent after:border-transparent' : ''
       } ${
         $isFocus
@@ -159,6 +160,8 @@
           ? 'text-transparent before:border-transparent after:border-transparent text-gray-500'
           : ''
       } ${$isEmpty ? 'leading-[4.6]' : 'leading-tight'}`}
+      class:before:border-gray-400={!$isEmpty}
+      class:after:border-gray-400={!$isEmpty}
     >
       {label}
     </label>
