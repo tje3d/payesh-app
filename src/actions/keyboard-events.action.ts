8c02517c96c7cae1dbd5b04f7b72389f28ba8260
Moein <tje3d@yahoo.com>
2023-10-01 01:06:54 +0330

    fix: Keyboard type

--- src/actions/keyboard-events.action.ts
+++ src/actions/keyboard-events.action.ts
@@ -1,35 +1,56 @@
 export default function KeyboardEvents(
-	node: HTMLElement,
-	keyType: 'keyup' | 'keydown' = 'keydown'
+  node: HTMLElement,
+  keyType: 'keyup' | 'keydown' = 'keydown',
 ) {
-	node.addEventListener(keyType, onKey);
+  node.addEventListener(keyType, onKey)
 
-	function onKey(e: KeyboardEvent) {
-		switch (e.key) {
-			case 'Escape':
-			case 'Enter':
-			case 'ArrowRight':
-			case 'ArrowDown':
-			case 'ArrowLeft':
-			case 'ArrowUp':
-			case 'Backspace':
-			case 'Delete':
-			case ' ':
-			case 'End':
-			case 'Home':
-			case 'PageUp':
-			case 'PageDown':
-			case 'Tab':
-				const key = e.key === ' ' ? 'Space' : e.key;
-				node.dispatchEvent(new CustomEvent(key, { detail: e }));
-				break;
-		}
-	}
+  function onKey(e: KeyboardEvent) {
+    switch (e.key) {
+      case 'Escape':
+      case 'Enter':
+      case 'ArrowRight':
+      case 'ArrowDown':
+      case 'ArrowLeft':
+      case 'ArrowUp':
+      case 'Backspace':
+      case 'Delete':
+      case ' ':
+      case 'End':
+      case 'Home':
+      case 'PageUp':
+      case 'PageDown':
+      case 'Tab':
+        const key = e.key === ' ' ? 'Space' : e.key
+        node.dispatchEvent(new CustomEvent(key, { detail: e }))
+        break
+    }
+  }
 
-	return {
-		destroy() {
-			// ...
-			node.removeEventListener(keyType, onKey);
-		}
-	};
+  return {
+    destroy() {
+      // ...
+      node.removeEventListener(keyType, onKey)
+    },
+  }
+}
+
+declare global {
+  namespace svelteHTML {
+    interface HTMLAttributes {
+      'on:Space'?: () => void
+      'on:Escape'?: () => void
+      'on:Enter'?: () => void
+      'on:ArrowRight'?: () => void
+      'on:ArrowDown'?: () => void
+      'on:ArrowLeft'?: () => void
+      'on:ArrowUp'?: () => void
+      'on:Backspace'?: () => void
+      'on:Delete'?: () => void
+      'on:End'?: () => void
+      'on:Home'?: () => void
+      'on:PageUp'?: () => void
+      'on:PageDown'?: () => void
+      'on:Tab'?: () => void
+    }
+  }
 }
